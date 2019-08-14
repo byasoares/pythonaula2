fc800a08020ffcf28a66e0d59f6a23bd57abbbda
byasoares <lovepack@hotmail.com.br>
2019-08-14 10:10:23 -0300

    numeros par ou impar negativo ou positivo

---
+++ numeros.py
@@ -0,0 +1,11 @@
+numero = int(input("Digite um número: "))
+
+if numero % 2 == 0:
+    print("Número par!")
+else :
+    print("Número inpar!")
+
+if numero >= 0:
+    print("Número positivo!")
+else:
+    print("Número negativo!")
